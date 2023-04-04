3bb7a429456aae11def00287f18493bbbdc274cb
fartun <fartunwario34@gmail.com>
2023-04-04 18:24:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,23 +1,104 @@
 /* Your Code Here */
-
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
-
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-    return payable
+function createEmployeeRecord(arr) {
+  return {
+    firstName: arr[0],
+    familyName: arr[1],
+    title: arr[2],
+    payPerHour: arr[3],
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+}
+function loadEmployeeRecord(empRecord, arr) {
+  empRecord.firstName = arr[0];
+  empRecord.familyName = arr[1];
+  empRecord.title = arr[2];
+  empRecord.payPerHour = arr[3];
+  empRecord.timeInEvents = [];
+  empRecord.timeOutEvents = [];
+  return empRecord;
 }
 
+
+function createEmployeeRecords(arr) {
+  return arr.map(function(empArr) {
+    return createEmployeeRecord(empArr);
+  });
+}
+function createTimeInEvent(dateStamp) {
+  const [date, hour] = dateStamp.split(' ');
+
+  if (!date || !hour) {
+    throw new Error('Invalid date format');
+  }
+
+  const timeInEvent = {
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+  };
+
+  this.timeInEvents.push(timeInEvent);
+
+  return this;
+}
+function createTimeOutEvent(dateStamp) {
+  const [date, hour] = dateStamp.split(' ')
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date: date,
+  })
+  return this
+}function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find(event => event.date === date);
+  const timeOut = this.timeOutEvents.find(event => event.date === date);
+  if (!timeIn || !timeOut) {
+    return undefined;
+  }
+  return (timeOut.hour - timeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(date) {
+  const hoursWorked = hoursWorkedOnDate.call(this, date);
+  if (hoursWorked === undefined) {
+    return undefined;
+  }
+  return hoursWorked * this.payPerHour;
+}
+
+function allWagesFor(empRecord) {
+  const datesWorked = empRecord.timeInEvents.map(event => event.date);
+  return totalWages;const employeeRecord = {
+    timeInEvents: {
+      "2023-04-01": { type: "TimeIn", hour: 9 },
+      "2023-04-02": { type: "TimeIn", hour: 10 },
+      "2023-04-03": { type: "TimeIn", hour: 11 }
+    },
+    timeOutEvents: {
+      "2023-04-01": { type: "TimeOut", hour: 17 },
+      "2023-04-02": { type: "TimeOut", hour: 18 },
+      "2023-04-03": { type: "TimeOut", hour: 19 }
+    },
+    wagesEarnedOnDate(date) {
+      const hoursWorked = (this.timeOutEvents[date].hour - this.timeInEvents[date].hour) / 100;
+      return hoursWorked * this.payPerHour;
+    },
+    payPerHour: 21
+  };
+  function allWagesFor() {
+    const availableDates = Object.keys(this.timeInEvents); // find available dates
+    const totalWages = availableDates.reduce((acc, date) => acc + wagesEarnedOnDate.call(this, date), 0); // accumulate pay for each date
+    return totalWages;
+  }
+  const totalWages = allWagesFor.call(employeeRecord);
+  console.log(totalWages); // output: 378
+
+}
+function findEmployeeByFirstName(srcArray, firstName) {
+  return srcArray.find(empRecord => empRecord.firstName === firstName);
+}
+function calculatePayroll(employeeRecords) {
+  return employeeRecords.reduce((totalPay, empRecord) => totalPay + allWagesFor(empRecord), 0);
+}
+    
