aaa68051373b8af92e17f9a1e1aa85387e2e079a
fartun <fartunwario34@gmail.com>
2023-04-09 11:42:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* Your Code Here */
 function createEmployeeRecord(arr) {
   return {
     firstName: arr[0],
@@ -7,24 +6,13 @@ function createEmployeeRecord(arr) {
     payPerHour: arr[3],
     timeInEvents: [],
     timeOutEvents: []
-  }
+  };
 }
-function loadEmployeeRecord(empRecord, arr) {
-  empRecord.firstName = arr[0];
-  empRecord.familyName = arr[1];
-  empRecord.title = arr[2];
-  empRecord.payPerHour = arr[3];
-  empRecord.timeInEvents = [];
-  empRecord.timeOutEvents = [];
-  return empRecord;
-}
-
 
 function createEmployeeRecords(arr) {
-  return arr.map(function(empArr) {
-    return createEmployeeRecord(empArr);
-  });
+  return arr.map(createEmployeeRecord);
 }
+
 function createTimeInEvent(dateStamp) {
   const [date, hour] = dateStamp.split(' ');
 
@@ -42,15 +30,18 @@ function createTimeInEvent(dateStamp) {
 
   return this;
 }
+
 function createTimeOutEvent(dateStamp) {
-  const [date, hour] = dateStamp.split(' ')
+  const [date, hour] = dateStamp.split(' ');
   this.timeOutEvents.push({
     type: "TimeOut",
     hour: parseInt(hour, 10),
     date: date,
-  })
-  return this
-}function hoursWorkedOnDate(date) {
+  });
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
   const timeIn = this.timeInEvents.find(event => event.date === date);
   const timeOut = this.timeOutEvents.find(event => event.date === date);
   if (!timeIn || !timeOut) {
@@ -67,38 +58,16 @@ function wagesEarnedOnDate(date) {
   return hoursWorked * this.payPerHour;
 }
 
-function allWagesFor(empRecord) {
-  const datesWorked = empRecord.timeInEvents.map(event => event.date);
-  return totalWages;const employeeRecord = {
-    timeInEvents: {
-      "2023-04-01": { type: "TimeIn", hour: 9 },
-      "2023-04-02": { type: "TimeIn", hour: 10 },
-      "2023-04-03": { type: "TimeIn", hour: 11 }
-    },
-    timeOutEvents: {
-      "2023-04-01": { type: "TimeOut", hour: 17 },
-      "2023-04-02": { type: "TimeOut", hour: 18 },
-      "2023-04-03": { type: "TimeOut", hour: 19 }
-    },
-    wagesEarnedOnDate(date) {
-      const hoursWorked = (this.timeOutEvents[date].hour - this.timeInEvents[date].hour) / 100;
-      return hoursWorked * this.payPerHour;
-    },
-    payPerHour: 21
-  };
-  function allWagesFor() {
-    const availableDates = Object.keys(this.timeInEvents); // find available dates
-    const totalWages = availableDates.reduce((acc, date) => acc + wagesEarnedOnDate.call(this, date), 0); // accumulate pay for each date
-    return totalWages;
-  }
-  const totalWages = allWagesFor.call(employeeRecord);
-  console.log(totalWages); // output: 378
-
+function allWagesFor() {
+  const availableDates = this.timeInEvents.map(event => event.date);
+  const totalWages = availableDates.reduce((acc, date) => acc + wagesEarnedOnDate.call(this, date), 0);
+  return totalWages;
 }
+
 function findEmployeeByFirstName(srcArray, firstName) {
   return srcArray.find(empRecord => empRecord.firstName === firstName);
 }
+
 function calculatePayroll(employeeRecords) {
-  return employeeRecords.reduce((totalPay, empRecord) => totalPay + allWagesFor(empRecord), 0);
+  return employeeRecords.reduce((totalPay, empRecord) => totalPay + allWagesFor.call(empRecord), 0);
 }
-    
